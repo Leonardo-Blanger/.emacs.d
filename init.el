a0f430110e467227706fcea94a5195f322e6463d
Leonardo Blanger <leonardoblanger@gmail.com>
2026-01-26 12:07:42 +0800

    Ignore .cache directory; remove commented out packages

--- init.el
+++ init.el
@@ -159,32 +159,3 @@
   :config
   (treemacs-indent-guide-mode t)
   (setq treemacs-no-png-images nil))
-
-;; (use-package lsp-mode
-;;   :init
-;;   (setq gc-cons-threshold 100000000)
-;;   (setq read-process-output-max (* 1024 1024)) ;; 1mb
-;;   (setq lsp-keymap-prefix "C-c l")
-;;   :config
-;;   (setq lsp-idle-delay 0.500)
-;;   :hook
-;;   (lsp-mode . lsp-enable-which-key-integration)
-;;   :commands
-;;   (lsp lsp-deferred))
-
-;; ;; optionally
-;; (use-package lsp-ui :commands lsp-ui-mode)
-;; ;; if you are helm user
-;; (use-package helm-lsp :commands helm-lsp-workspace-symbol)
-;; ;; if you are ivy user
-;; (use-package lsp-ivy :commands lsp-ivy-workspace-symbol)
-;; (use-package lsp-treemacs :commands lsp-treemacs-errors-list)
-
-;; ;; optionally if you want to use debugger
-;; (use-package dap-mode)
-;; ;; (use-package dap-LANGUAGE) to load the dap adapter for your language
-
-;; ;; optional if you want which-key integration
-;; (use-package which-key
-;;     :config
-;;     (which-key-mode))
